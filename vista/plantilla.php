816aafd6012f4078c4db5b7c2694b6512707bd25
XenaEstupinan <xena040404@gmail.com>
2023-10-25 08:13:09 -0500

    edicion de la plantilla

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -1,13 +1,5 @@
 <?php
 
-include_once "vista/modulos/cabecera.php";
-
-include_once "vista/modulos/registro.php";
-include_once "vista/modulos/login.php";
-
-include_once "vista/modulos/pie.php";
-
-
 session_start();
 
 include_once "vista/modulos/cabecera.php";
