72479064112171cbc7294c7dfa16baf3952763b7
XenaEstupinan <xena040404@gmail.com>
2023-10-25 08:11:46 -0500

    edicion de cabecera, plantilla, ids del login, java del login

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -6,3 +6,25 @@ include_once "vista/modulos/registro.php";
 include_once "vista/modulos/login.php";
 
 include_once "vista/modulos/pie.php";
+
+
+session_start();
+
+include_once "vista/modulos/cabecera.php";
+
+if (isset($_SESSION["ruta"])) {
+
+    include_once "vista/modulos/inicio.php";
+
+    if($_GET["ruta"] == "registro" ||
+       $_GET["ruta"] == "login" ||
+       $_GET["ruta"] == "cerrarSesion"){
+
+    include_once "vista/modulos/".$_GET["ruta"].".php";
+    }
+} else {
+    include_once "vista/modulos/login.php";
+}
+
+include_once "vista/modulos/pie.php";
+
