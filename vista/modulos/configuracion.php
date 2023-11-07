e9a3da3d616cb65e3fecb12fa4d89ba0c3adcb7d
DrSamus <148347020+DrSamus@users.noreply.github.com>
2023-11-07 09:11:02 -0500

    Delete vista directory

--- vista/modulos/configuracion.php
+++
@@ -1,5 +0,0 @@
-<div class="container">
-    <td class="">
-        <img src="vista/imagenes/logoo.png" style="height: 350px; width:350px;">
-    </td>
-</div>
--- vista/modulos/inicio.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-include_once "vista/modulos/nav.php";
-include_once "vista/modulos/jumbutron.php";
-
