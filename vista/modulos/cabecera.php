e0b2e88581caeaa24e088e1a938ed696881abcff
Samuel <samuelmuan31@gmail.com>
2023-11-09 08:13:03 -0500

    Creacion de formulario para envios de correo

--- vista/modulos/cabecera.php
+++ vista/modulos/cabecera.php
@@ -16,6 +16,8 @@
   <!-- Latest compiled JavaScript -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
+  <!-- Bootstrap 5 Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
   <!-- Local CDN's -->
 
@@ -35,6 +37,10 @@
   <script src='vista/librerias/js/login.js'></script>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+  <!-- Jquery and Sweetalert -->
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
 
 </head>
--- vista/modulos/mail.php
+++ vista/modulos/mail.php
@@ -1,10 +1,14 @@
+<?php
+include 'control/mailControl.php';
+?>
+
 <div class="container">
 
 
-
+<!-- Primer container -->
     <div class="row">
-      <div class="col-md-5 bg-gray mr mb-5">
-        <h2>¡Comparte tu Hoja de Vida!</h2>
+      <div class="col-md-5 bg-orange mr mb-5 rounded">
+        <h2>¡Comparte tu hoja de vida con nosotros!</h2><hr>
         <p style="font-size:large;">Puedes compartir por correo tu hoja de vida previamente descargada como PDF.</p>
         <div style="display: flex; justify-content: center;">
         <img  style="width: 280px;" src="vista\imagenes\Koala_PDF-removebg-preview.png">
@@ -13,27 +17,47 @@
 
 
 
-
-      <div class="col-md-5 mb-5 bg-blue">
-        <h2>Ingresa tu hoja de vida Aquí.</h2>
+<!-- Segundo container -->
+      <div class="col-md-5 mb-5 bg-gray rounded">
         <div class="row">
             <div class="contact-section">
-                <div class="contact-form mx-auto col-md-6">
-                    <h2 style="color:black;">Enviar correo (GMAIL) por PHP.</h2>
-                    <form class="contact" action="index.php" method="post">
+                <div class="contact-form mx-auto">
+                    <h2 style="color:black;">¡Adjunta tu hoja de vida!</h2><hr>
+                    <div class="container bg-white rounded">
+                        
+                    <!-- Formulario  -->
+                    <form class="contact" action="" method="post" enctype="multipart/form-data">
+
+                        <!-- Input Email Address -->
                         <div class="mb-3">
-                            <input type="text" name="name" class="form-control" placeholder="Nombre" required>
+                            <input type="email" name="destinatario" class="form-control" placeholder="Escribe el correo del destinatario:" required>
                         </div>
+
+                        <!-- Input Asunto Correo -->
                         <div class="mb-3">
-                            <input type="email" name="email" class="form-control" placeholder="Correo Electrónico" required>
+                            <input type="text" name="asunto" class="form-control" placeholder="Escribe el asunto del correo:" required>
                         </div>
+
+                        <!-- Textarea Contenido Correo Electronico -->
                         <div class="mb-3">
-                            <textarea name="message" rows="5" class="form-control" placeholder="Mensaje" required></textarea>
+                            <textarea name="contenido" rows="5" class="form-control" placeholder="Escribe el contenido del correo:" required></textarea>
                         </div>
+
+                        <!-- Files Input -->
                         <div class="mb-3">
-                            <input type="submit" name="submit" class="btn btn-danger text-dark" value="Enviar">
+                            <label for="formFile" class="form-label">Adjunta tu hoja de vida (PDF).</label>
+                            <input class="form-control" type="file" id="formFile" name="file">
                         </div>
+
+                        <!-- Boton Enviar -->
+                        <div class="mb-3">
+                            <button type="submit" name="submit" class="btn btn-danger text-dark">
+                                Enviar <i class="bi bi-send"></i>
+                            </button>
+                        </div>
+
                     </form>
+                    </div>
                 </div>
             </div>
             </div>
