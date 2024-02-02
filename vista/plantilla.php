18bf22de00d26f728cb1ba0319f555f7b669b5f1
XenaEstupinan <xena040505@gmail.com>
2024-02-01 21:25:15 -0500

    Arreglo de plantilla para no ingresar sin inicio de sesion

--- vista/plantilla.php
+++ vista/plantilla.php
@@ -4,39 +4,38 @@ session_start();
 include_once "vista/modulos/cabecera.php";
 include_once "vista/modulos/nav.php";
 
-// Comprobar si $_GET["ruta"] está definido
-if (isset($_GET["ruta"])) {
-    if (!isset($_SESSION["ruta"])) {
-        if ($_GET["ruta"] == "login" 
-            || $_GET["ruta"] == "registro" 
-            || $_GET["ruta"] == "mail" 
-            || $_GET["ruta"] == "formatoSena"
-            || $_GET["ruta"] == "principal"
-            || $_GET["ruta"] == "formato"
-            || $_GET["ruta"] == "formato2") {
-
-            include_once "vista/modulos/".$_GET["ruta"].".php";
+// Comprobar si hay una sesión iniciada
+if (!isset($_SESSION["ruta"])) {
+    // Comprobar si $_GET["ruta"] está definido
+    if (isset($_GET["ruta"])) {
+        if ($_GET["ruta"] == "login" || $_GET["ruta"] == "registro") {
+            include_once "vista/modulos/" . $_GET["ruta"] . ".php";
         } else {
-            // Si $_GET["ruta"] no está definido, se puede manejar de manera predeterminada aquí
-            // Por ejemplo, puedes incluir la página principal o una página de inicio aquí.
-            include_once "vista/modulos/inicio.php";
+            // Si $_GET["ruta"] no está definido o no es "login" o "registro",
+            // redirigir al login (o cualquier otra acción que desees)
+            include_once "vista/modulos/login.php";
+            
         }
-
     } else {
-        if ($_GET["ruta"] == "cerrarSesion") {
-            include_once "vista/modulos/".$_GET["ruta"].".php";
-        } else {
-            include_once "vista/modulos/".$_GET["ruta"].".php";
-            //include_once "vista/modulos/jumbutron.php";
-            // Otros archivos que desees incluir después del inicio de sesión
-        }
+        // Si $_GET["ruta"] no está definido, se puede manejar de manera predeterminada aquí
+        // Por ejemplo, puedes incluir la página principal o una página de inicio aquí.
+        include_once "vista/modulos/inicio.php";
     }
 } else {
-    // Si $_GET["ruta"] no está definido, se puede manejar de manera predeterminada aquí
-    // Por ejemplo, puedes incluir la página principal o una página de inicio aquí.
-    include_once "vista/modulos/inicio.php";
+    // Si ya hay una sesión iniciada, permitir el acceso a otras rutas
+    if (isset($_GET["ruta"])) {
+        if ($_GET["ruta"] == "cerrarSesion") {
+            include_once "vista/modulos/" . $_GET["ruta"] . ".php";
+        } else {
+            include_once "vista/modulos/" . $_GET["ruta"] . ".php";
+            // Otros archivos que desees incluir después del inicio de sesión
+        }
+    } else {
+        // Si $_GET["ruta"] no está definido, se puede manejar de manera predeterminada aquí
+        // Por ejemplo, puedes incluir la página principal o una página de inicio aquí.
+        include_once "vista/modulos/inicio.php";
+    }
 }
-
 
 include_once "vista/modulos/pie.php";
 ?>
